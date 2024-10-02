e7f7f6259fd80e998a1f06f1e84ad13c1bf259d6
jmfp <therealjmfp@gmail.com>
2024-10-02 00:45:35 -0400

    fixed

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import Image from "next/image"
-import { deleteBlog, getAllCourses, getAllPosts } from "@/actions/actions"
+import { deleteBlog, getAllPosts } from "@/actions/actions"
 import { redirect } from "next/navigation"
 
 export default async function Dashboard(){
     
-    const [posts, courses] = await Promise.all([getAllPosts(), getAllCourses()])
+    const [posts] = await Promise.all([getAllPosts()])
     //const posts = await getAllPosts()
     //const courses = await getAllCourses()
 
@@ -42,45 +42,6 @@ export default async function Dashboard(){
                         </div>
                     ))
                 }
-            </div>
-            <div className='my-2'>
-                <p>All Courses</p>
-            </div>
-            <div className="w-[80%] h-[800px] justify-center rounded-lg border-2 border-purple-500 overflow-y-scroll">
-                {!courses.length ? <span/> : 
-                    courses.map((course: any, idx: number) =>(
-                        <div key={idx} className="display: flex w-[80%] m-auto gap-4 border-2 border-purple-500  justify-between items-center rounded-lg my-4">
-                            <Image 
-                                src={course.image} 
-                                alt={course.title} 
-                                width={200} 
-                                height={200}
-                                className="h-[100px] w-[100px] rounded-lg object-cover"
-                            />
-                            <p>{course.title}</p>
-                            <div className="display: flex gap-4 m-auto">
-                                <form action={async () =>{
-                                    'use server'
-                                    //edit the course
-                                    await redirect(`/admin/courses/edit/${course.id}`)
-                                    //await deleteBlog(post.slug)
-                                    //await deleteBlog
-                                }}>
-                                    <Button type='submit'>Edit</Button>
-                                </form>
-                                <Button>Edit</Button>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-            <div className="my-2 flex flex-row w-[80%] p-4 gap-2">
-                <Button asChild className="m-auto">
-                    <Link href="/admin/posts/new">New Post</Link>
-                </Button>
-                <Button asChild className="m-auto">
-                    <Link href="/admin/courses/new">New Course</Link>
-                </Button>
             </div>
         </div>
     )
