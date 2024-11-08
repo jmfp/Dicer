7859681990d69724f2e256edd6e08e9b2cae0353
jmfp <therealjmfp@gmail.com>
2024-11-08 02:55:04 -0500

    sitemap

--- app/item/[item]/[platform]/page.tsx
+++ app/item/[item]/[platform]/page.tsx
@@ -190,7 +190,6 @@ export default async function ShopItem({params}: {params: {item: string, platfor
             </div>
           </div>
         </LitContainer>
-        
         {//TODO: change all content checks to check for status 400
         screenshots[0].status == 400 ? null : 
         <Carousel className="p-6 mx-12">
