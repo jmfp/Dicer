962229e0d8539841a7c7cc4b861c01d80cc27993
jmfp <therealjmfp@gmail.com>
2024-11-09 19:39:04 -0500

    sittemap

--- app/item/[item]/[platform]/page.tsx
+++ app/item/[item]/[platform]/page.tsx
@@ -242,6 +242,7 @@ export default async function ShopItem({params}: {params: {item: string, platfor
           <CarouselNext />
         </Carousel>
         }
+        
         {video ? 
           video.map((vid : any, idx: number) =>{
             return(
